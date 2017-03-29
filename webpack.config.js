437b4c11c4c41486da99bb7fde370c1220941a27
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-03-29 09:07:26 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+
+    ]
+  },
+  babel: {
+
+  },
+  plugins: [
+
+  ]
+}
