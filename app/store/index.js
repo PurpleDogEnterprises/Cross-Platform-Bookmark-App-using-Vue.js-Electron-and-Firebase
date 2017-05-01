908b20b7ba176d4a22911d554e124202245fd30f
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-05-01 12:36:29 -0400

    Update index.js

--- app/store/index.js
+++ app/store/index.js
@@ -1,2 +1,10 @@
 import { EventEmitter } from 'events'
 import Firebase from 'firebase'
+
+const db = new Firebase("https://bookmarking-app-486da.firebaseio.com/")
+const categoriesRef = db.child('categories')
+const bookmarksRef = db.child('bookmarks')
+const store = new EventEmitter()
+
+let categories = {}
+let bookmarks = {}
