7381d90847527756fd21f3ad6d7fb39658954459
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-05-03 07:36:56 -0400

    Update index.js

--- app/store/index.js
+++ app/store/index.js
@@ -8,3 +8,16 @@ const store = new EventEmitter()
 
 let categories = {}
 let bookmarks = {}
+
+db.on('value', (snapshot) => {
+  var bookmarkData = snapshot.val()
+  if (bookmarkData) {
+    categories = bookmarkData.categories
+    bookmarks = bookmarkData.bookmarks
+    store.emit('data-updated', categories, bookmarks)
+  }
+})
+
+store.addCategory = (category) => {
+  categoriesRef.update(category)
+}
